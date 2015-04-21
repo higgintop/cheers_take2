dcb27244863c569af22b483f46359129f74791f0
Allison Higginbotham <higgintop@gmail.com>
2015-04-21 13:38:43 -0500

    Fixed indentation issues on ruby files

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -1,14 +1,14 @@
 require 'minitest/autorun'
 
 class TestCheersIntegration < Minitest::Test
-	
-	def test_with_no_arguments
-		output = `./cheers`
-		expected = <<EOS
+
+  def test_with_no_arguments
+    output = `./cheers`
+    expected = <<EOS
 I'd cheer for you, if only I knew who you were :(
 Try again with `./cheers [Name] [mm/dd Birthday]`
 EOS
-		assert_equal expected, output
-	end
+    assert_equal expected, output
+  end
 
 end
