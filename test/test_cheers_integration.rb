e81848c679166c72a011d2bcf24d6cd66901796c
Allison Higginbotham <higgintop@gmail.com>
2015-04-21 13:29:54 -0500

    First test with no cmd line arguments passing

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -2,8 +2,13 @@ require 'minitest/autorun'
 
 class TestCheersIntegration < Minitest::Test
 	
-	def test_that_the_tests_run
-		assert_equal true, false
+	def test_with_no_arguments
+		output = `./cheers`
+		expected = <<EOS
+I'd cheer for you, if only I knew who you were :(
+Try again with `./cheers [Name] [mm/dd Birthday]`
+EOS
+		assert_equal expected, output
 	end
 
 end
